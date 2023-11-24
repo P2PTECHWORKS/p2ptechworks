665bc59d810e06441aeeed0e0e72ef22b3f5fab0
P2PTECHWORKS <144785714+P2PTECHWORKS@users.noreply.github.com>
2023-11-24 20:29:19 +0530

    Update ChatbotIcon.js

--- src/components/ChatbotIcon/ChatbotIcon.js
+++ src/components/ChatbotIcon/ChatbotIcon.js
@@ -29,7 +29,7 @@ const ChatbotIcon = () => {
     const userMessage = { text: input, user: 'user' };
     setMessages((prevMessages) => [...prevMessages, userMessage]);
     
-    const backendURL = 'http://192.168.1.5:5000';
+    const backendURL = 'https://booming-premise-406113.el.r.appspot.com/';
 
 
     try {
