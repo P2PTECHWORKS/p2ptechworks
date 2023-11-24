efd6b28b9bd7791d47fb9ac446ea57f22b17b377
P2PTECHWORKS <144785714+P2PTECHWORKS@users.noreply.github.com>
2023-11-24 12:26:43 +0530

    Update index.js

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -30,13 +30,21 @@ const BodySection = () => {
                 <ReadMore>
                     <div className="container">
 
-                        <a href="" className="button">
-                            <div className="button__line"></div>
-                            <div className="button__line"></div>
-                            <span className="button__text">READ MORE</span>
-                            <div className="button__drow1"></div>
-                            <div className="button__drow2"></div>
-                        </a>
+                        // <a href="" className="button">
+                        //     <div className="button__line"></div>
+                        //     <div className="button__line"></div>
+                        //     <span className="button__text">READ MORE</span>
+                        //     <div className="button__drow1"></div>
+                        //     <div className="button__drow2"></div>
+                        // </a>
+                        <button className="button">
+    <div className="button__line"></div>
+    <div className="button__line"></div>
+    <span className="button__text">READ MORE</span>
+    <div className="button__drow1"></div>
+    <div className="button__drow2"></div>
+</button>
+
 
                     </div>
                 </ReadMore>
